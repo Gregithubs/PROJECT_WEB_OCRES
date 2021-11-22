575e01ff154a935dfc52132885fa431b2a443a5c
Gregithubs <greg.pateyron@gmail.com>
2021-11-22 15:07:48 +0100

    Dashboard V1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component }  from 'react';
 import './App.css';
+
+const Box = (props) => {
+  const style = props.style?props.style:{}
+  return (
+    <div style={{...style, backgroundColor:'white', color: "black"}}>
+      {props.children}
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{backgroundColor: "grey"}}>Header</div>
+        <main style={{display: "flex", gap: "1rem", height: "100%"}}>
+          <nav style={{backgroundColor: "grey", maxWidth: "195px"}}>
+            <ul>
+              <li>Some</li>
+              <li>Elements</li>
+              <li>Some</li>
+              <li>Elements</li>
+            </ul>x
+          </nav>
+          <div style={{marginTop: "1rem", display: "grid", gridGap: '1rem', gridTemplateColumns: "repeat(6, 1fr)", width: "100%"}}>
+            <Box style={{gridColumnStart: 1, gridColumnEnd: 3}}>1</Box>
+            <Box style={{gridColumnStart: 3, gridColumnEnd: 5}}>2</Box>
+            <Box style={{gridColumnStart: 5, gridColumnEnd: 7}}>3</Box>
+            <Box style={{gridColumnStart: 1, gridColumnEnd: 4, gridRow: "2 / span 6"}}>4</Box>
+            <Box style={{gridColumnStart: 4, gridColumnEnd: 7, gridRow: "2 / span 6"}}>5</Box>
+            <Box style={{gridColumnStart: 1, gridColumnEnd: 7, gridRow: "8 / span 12"}}>6</Box>
+          </div>
+          <nav style={{maxWidth: "195px"}}>
+            <ul>
+              <li>Some</li>
+              <li>Elements</li>
+              <li>Some</li>
+              <li>Elements</li>
+            </ul>
+          </nav>
+        </main>
       </header>
     </div>
   );
