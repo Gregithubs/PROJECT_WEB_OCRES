3323c686e52494587f84a9979fdece695a17a5db
emilien <emilienbiron@gmail.com>
2021-11-26 04:06:09 +0100

    Page utilisateur

--- frontend/src/templates/User.js
+++ frontend/src/templates/User.js
@@ -1,88 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import axios from "axios"
-import millify from "millify"
-
-import SummaryCard from "../Components/SummaryCard"
+import React from 'react';
 import Layout from "../Components/Layout"
-
-
-import { apiKey } from "../api/nomicsApi"
-
-const User = () => {
-  const [dataApi, setDataApi] = useState(null)
-  const [dataApi2, setDataApi2] = useState(null)
-
-  useEffect(() => {
-    axios.get(`https://api.nomics.com/v1/currencies/ticker?key=${apiKey}&ids=BTC,ETH,XRP&interval=1d,30d&convert=EUR&per-page=100&page=1`)
-      .then((res) => {
-        const items = res.data;
-        setDataApi(items);
-      })
-
-    axios.get("https://coinranking1.p.rapidapi.com/stats", {
-      headers: {
-        'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-        'x-rapidapi-key': 'c75bfb7a18msh8c51fe9e891ca40p104a36jsnfb1c942a4971'
-      }
-    })
-      .then((res) => {
-        const items = res.data.data;
-        setDataApi2(items);
-      })
-
-  }, []);
+var image=require('./jeandidier.jpg')
+const Informations = () => {
   return (
-    <Layout>
-      <main className="widgets">
-        {dataApi &&
-          (
-            <div className="row1">
-              <SummaryCard currencyData={dataApi[0]} />
-              <SummaryCard currencyData={dataApi[0]} />
-              <SummaryCard currencyData={dataApi[0]} />
-            </div>
-          )
-        }
-        {dataApi &&
-          (
-            <div className="row2">
-              <SummaryCard currencyData={dataApi[0]} />
-              <SummaryCard currencyData={dataApi[0]} />
-            </div>
-          )
-        }
-        {dataApi &&
-          (
-            <div className="row3">
-              <SummaryCard currencyData={dataApi[0]} />
-            </div>
-          )
-        }
-      </main>
-      <nav className="total">
-        <ul>
-          <a>Total <br />Cryptocurrencies</a><br />
-          <a></a>
-          {dataApi2 ? millify(dataApi2.totalCoins) : null}
-          <br /><br />
-          <a>Total Exchanges</a><br />
-          <a></a>
-          {dataApi2 ? millify(dataApi2.totalExchanges) : null}
-          <br /><br />
-          <a>Total Market Cap</a><br />
-          <a></a>
-          {dataApi2 ? `$ ${millify(dataApi2.totalMarketCap)}` : null}
-          <br /><br />
+        <Layout>
+            <textearea className="about">
+                <h1>Profil</h1><br />
+            Bienvenue sur votre Profil ! Ici vous pourrez voir votre portefeuille et son évolution. Vous pourrez aussi aller regarder le portefeuilles de vos amis et voir si leurs investissements se portent bien!<br /><br />
+            <h1>Informations</h1><br />
+            Jean-Didier CryptoTrader
+           <img class="jeandidier" src={image} alt="la tête de jean-didier notre meilleur client" /> <br />
+            Homme <br />
+            36 ans <br />
+            Vis à : Paris, France<br />
+            Célibataire<br />
 
-          <a>Total 24h Volume</a><br />
-          <a></a>
-          {dataApi2 ? `$ ${millify(dataApi2.total24hVolume)}` : null}
-          <br /><br />
+            <h1>Numéro de téléphone</h1><br />
+            +33 7 99 99 99 91<br /><br />
 
-        </ul>
-      </nav>
-    </Layout>
+            <h1>Adresse mail</h1><br />
+            jeandidieradorelacryptomonnaie@gmail.com<br />
+            </textearea>
+
+            <textearea className="amis">
+            Vos Amis :
+            </textearea>
+        </Layout>
   )
 }
 
-export default User;
+export default Informations;
