853b663cc0f6b848cc9b1543238e2e87be800c70
emilien <emilienbiron@gmail.com>
2021-11-26 11:27:45 +0100

    Corrections user

--- frontend/src/templates/User.js
+++ frontend/src/templates/User.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import Layout from "../Components/Layout"
+import axios from "axios"
+import millify from "millify"
+
 var image=require('./jeandidier.jpg')
-const Informations = () => {
+const User = () => {
   return (
         <Layout>
-            <textearea className="about">
-                <h1>Profil</h1><br />
+            <textearea className="about-user">
+                <h1>Page de profil</h1><br />
             Bienvenue sur votre Profil ! Ici vous pourrez voir votre portefeuille et son évolution. Vous pourrez aussi aller regarder le portefeuilles de vos amis et voir si leurs investissements se portent bien!<br /><br />
-            <h1>Informations</h1><br />
+            <h1>Infos</h1><br />
             Jean-Didier CryptoTrader
            <img class="jeandidier" src={image} alt="la tête de jean-didier notre meilleur client" /> <br />
             Homme <br />
             36 ans <br />
             Vis à : Paris, France<br />
             Célibataire<br />
-
-            <h1>Numéro de téléphone</h1><br />
             +33 7 99 99 99 91<br /><br />
-
-            <h1>Adresse mail</h1><br />
             jeandidieradorelacryptomonnaie@gmail.com<br />
             </textearea>
 
@@ -29,4 +28,4 @@ const Informations = () => {
   )
 }
 
-export default Informations;
+export default User;
