d2fcf0ec3b1218f2dbdb7f28d5f148c7f65a67d3
Gregithubs <greg.pateyron@gmail.com>
2021-11-26 12:41:45 +0100

    News modif

--- frontend/src/templates/News.js
+++ frontend/src/templates/News.js
@@ -4,29 +4,20 @@ import axios from "axios"
 import SummaryCard from "../Components/SummaryCard"
 import Layout from "../Components/Layout"
 
-
-import { apiKey } from "../api/nomicsApi"
-
 const News = () => {
   const [dataApi, setDataApi] = useState(null)
-  const [dataApi2, setDataApi2] = useState(null)
 
   useEffect(() => {
-    axios.get(`https://api.nomics.com/v1/currencies/ticker?key=${apiKey}&ids=BTC,ETH,XRP&interval=1d,30d&convert=EUR&per-page=100&page=1`)
-      .then((res) => {
-        const items = res.data;
-        setDataApi(items);
-      })
-
-    axios.get("https://coinranking1.p.rapidapi.com/stats", {
+    axios.get("https://bing-news-search1.p.rapidapi.com/news", {
       headers: {
-        'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+        'x-bingapis-sdk': 'true',
+        'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
         'x-rapidapi-key': 'c75bfb7a18msh8c51fe9e891ca40p104a36jsnfb1c942a4971'
       }
     })
       .then((res) => {
         const items = res.data.data;
-        setDataApi2(items);
+        setDataApi(items);
       })
 
   }, []);
@@ -51,16 +42,6 @@ const News = () => {
             </div>
           )
         }
-        {dataApi &&
-          (
-            <div className="row1">
-              <SummaryCard currencyData={dataApi[0]} />
-              <SummaryCard currencyData={dataApi[0]} />
-              <SummaryCard currencyData={dataApi[0]} />
-            </div>
-          )
-        }
-        
       </main>
     </Layout>
   )
