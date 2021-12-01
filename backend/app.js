c8ab169cb424d9f6e06cdb1a80fb20480ffbef81
emilien <emilienbiron@gmail.com>
2021-12-01 10:18:36 +0100

    back+css

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,29 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+const hostname="localhost"
+const port=3001
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+
+app.get ('/test', (req,res)=> {
+    res.json({
+        req: req.method,
+        data:"This is a GET"
+    })
+});
+app.put('/test',(req,res)=>{
+    res.json({
+        req: req.method,
+        data:"This is a PUT"
+    });
+});
+
+app.listen(port,hostname),() =>{
+    console.log(`mon serveur fonctionne sur http://${hostname}:${port}\n`);
+}
 
 app.use(logger("dev"));
 app.use(express.json());
