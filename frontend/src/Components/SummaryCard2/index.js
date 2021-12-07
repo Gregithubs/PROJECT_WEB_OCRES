a1e213301d5385f9816290c8eafc5ffc54442cdd
Gregithubs <greg.pateyron@gmail.com>
2021-12-07 14:27:39 +0100

    arround wallet

--- frontend/src/Components/SummaryCard2/index.js
+++ frontend/src/Components/SummaryCard2/index.js
@@ -37,7 +37,7 @@ const SummaryCard2 = ({ currencyData = {
             </div>
             <div className="info-container">
                 <p>Price : $ {priceFormatted}</p>
-                <p>Amount : {num_exchanges*0.001}</p>
+                <p>Amount : {(Math.round(num_exchanges * 0.1) / 100).toFixed(2)}</p>
                 <p>Solde : $ {soldeFormatted}</p>
             </div>
         </div>
